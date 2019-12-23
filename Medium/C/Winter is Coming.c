07a0e4830f63c543835580d162f2ddc0809e5f12
Aimnos <roikegami@hotmail.com>
2019-12-23 16:36:51 -0300

    First solution implementation

---
+++ Medium/C/Winter is Coming.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main() {
+  int T, N, i, *a, x, y, j;
+  scanf("%d", &T);
+  for(i = 0; i < T; i++) {
+    scanf("%d", &N);
+    a = (int*)malloc(N*sizeof(int));
+    x = 0;
+    y = 0;
+    for(j = 0; j < N; j++)
+      scanf("%d", &a[j]);
+
+    for(j = 0; j < N; j++) {
+      if(j%2 == 0)
+        x += a[j];
+      else
+        y += a[j];
+
+    }
+    if(x >= y)
+      printf("%d\n", x);
+    else
+      printf("%d\n", y);
+
+    free(a);
+  }
+  return 0;
+}

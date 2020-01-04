0533d8b5e46227ed3b196b03239809a8094b1332
Aimnos <roikegami@hotmail.com>
2020-01-04 11:00:55 -0300

    First solution implementation

---
+++ Hard/C/CashBack.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main() {
+  long int N, C, X, M = 0, y = 0, i, lowest = 100000, highest = 1;
+  scanf("%li", &N);
+  for(i = 0; i < N; i++) {
+    scanf("%li %li", &C, &X);
+    M += C;
+    M -= X;
+    if(X < lowest || lowest < 0)
+      lowest = X;
+
+    if(C > highest)
+      highest = C;
+
+  }
+  if(M + lowest > highest)
+    printf("%li", M + lowest);
+  else
+    printf("%li", highest);
+
+  return 0;
+}

0a6cda7fb62a389fe8199273aa0a90ddc8c26a5f
Aimnos <roikegami@hotmail.com>
2021-07-06 17:51:23 -0300

    Removed extra space

--- Easy/C/Leap Year.c
+++ Easy/C/Leap Year.c
@@ -4,7 +4,7 @@ int main() {
 	unsigned char T;
 	unsigned int Year;
 	scanf("%hhu", &T);
-	for(unsigned char i =  0; i < T; i++) {
+	for(unsigned char i = 0; i < T; i++) {
 		scanf("%u", &Year);
 		if(Year % 400 == 0 || (Year % 4 == 0 && Year % 100 != 0))
 			printf("Yes\n");

4a960232d5d4a77b6d5b40e065f8be17512a6fdf
= <=>
2021-07-10 08:03:41 -0300

    Code optimization

--- Hard/Rust/Alice and Dictionary.rs
+++ Hard/Rust/Alice and Dictionary.rs
@@ -2,47 +2,28 @@ use std::{io::{self, BufRead}, collections::HashSet};
 
 fn main() {
 	let stdin = io::stdin();
-	let mut lines = stdin.lock().lines();
+	let lines = &mut stdin.lock().lines();
 	let T: u8 = lines.next().unwrap().unwrap().trim().parse().unwrap();
 	for _ in 0..T {
 		let line = lines.next().unwrap().unwrap();
 		let N = line.trim();
 		let L: u16 = lines.next().unwrap().unwrap().trim().parse().unwrap();
-		let mut words = HashSet::with_capacity(L as usize);
-		for _ in 0..L {
-			let line = lines.next().unwrap().unwrap();
-			words.insert(String::from(line.trim()));
-		}
-		let mut highest: u8 = 0;
-		for word in &words {
-			let mut count: u8 = 0;
-			if N.starts_with(word.as_str()) {
-				count += 1;
-				search_for_substring(N.split_at(word.len()).1, &words, &mut count);
-				if count > highest {
-					highest = count;
-				}
-			}
-		}
-		println!("{}", highest);
+		println!("{}", search_for_substring(N, &lines.map(|line| line.unwrap().trim().to_owned()).take(L as usize).collect::<HashSet<String>>(), 0));
 	}
 }
 
-fn search_for_substring(string: &str, words: &HashSet<String>, count: &mut u8) {
-	let first = *count;
-	let mut highest = *count;
+fn search_for_substring(string: &str, words: &HashSet<String>, first: u8) -> u8 {
+	let mut highest = first;
 	for word in words {
-		*count = first;
 		if string.starts_with(word) {
-			*count += 1;
-			search_for_substring(string.split_at(word.len()).1, words, count);
-			if *count > highest {
-				highest = *count;
+			let count = search_for_substring(string.split_at(word.len()).1, words, first + 1);
+			if count > highest {
+				highest = count;
 			}
 		}
 	}
 	if string.len() > 0 && highest == first {
 		highest -= 1;
 	}
-	*count = highest;
+	highest
 }

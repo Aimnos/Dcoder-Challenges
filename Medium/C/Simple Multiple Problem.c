d5f9956f0992802218b9523f18abde2c6ac33948
= <=>
2021-07-14 20:12:13 -0300

    Refactored code

--- Medium/C/Simple Multiple Problem.c
+++ Medium/C/Simple Multiple Problem.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 int main() {
-	int T;
-	long int N, M, n1, n2, aux;
-	scanf("%d", &T);
-	for(int i = 0; i < T; i++) {
-		scanf("%ld %ld", &N, &M);
+	unsigned char T;
+	unsigned int N, M, n1, n2, aux;
+	scanf("%hhu", &T);
+	for(unsigned char i = 0; i < T; i++) {
+		scanf("%u %u", &N, &M);
 		n1 = N;
 		n2 = M;
 		if(n2 > n1) {
-		aux = n1;
-		n1 = n2;
-		n2 = aux;
-		}
-		while(n2 > 0) {
-			aux = n1%n2;
+			aux = n1;
 			n1 = n2;
 			n2 = aux;
 		}
-		printf("%ld\n", M/n1);
+		while(n2 > 0) {
+			aux = n1 % n2;
+			n1 = n2;
+			n2 = aux;
+		}
+		printf("%u\n", M / n1);
 	}
 	return 0;
 }

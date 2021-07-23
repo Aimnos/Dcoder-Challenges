4960ba462d0927c3b7e70d5a227cd5d496a9c6a5
Aimnos <roikegami@hotmail.com>
2021-07-23 13:53:42 -0300

    First solution implementation

--- Hard/Rust/CashBack.rs
+++ Hard/Rust/CashBack.rs
@@ -1,14 +1,35 @@
-use std::io::{self, BufRead};
+use std::{
+    io::{self, BufRead},
+    iter::once,
+};
 
 fn main() {
-	let stdin = io::stdin();
-	let mut lines = stdin.lock().lines();
-	let N: u32 = lines.next().unwrap().unwrap().trim().parse().unwrap();
-	let CiXi: Vec<(u32, u32)> = lines.take(N as usize).map(|line| {
-		let line = line.unwrap();
-		let mut values = line.split_whitespace();
-		(values.next().unwrap().parse::<u32>().unwrap(), values.next().unwrap().parse::<u32>().unwrap())
-	}).collect::<Vec<_>>();
-	let lowest = CiXi.iter().min().unwrap();
-	let highest = CiXi
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+    let N: u32 = lines.next().unwrap().unwrap().trim().parse().unwrap();
+    let CiXi: Vec<_> = lines
+        .take(N as usize)
+        .map(|line| {
+            let line = line.unwrap();
+            let mut values = line.split_whitespace();
+            (
+                values.next().unwrap().parse::<u32>().unwrap(),
+                values.next().unwrap().parse::<u32>().unwrap(),
+            )
+        })
+        .flat_map(|tup| once(tup.0).chain(once(tup.1)))
+        .collect();
+    let M = CiXi
+        .iter()
+        .step_by(2)
+        .fold(*CiXi.iter().skip(1).step_by(2).min().unwrap(), |acc, ci| {
+            acc + ci
+        }) as u64
+        - CiXi.iter().skip(1).step_by(2).fold(0, |acc, xi| acc + xi) as u64;
+    let highest = *CiXi.iter().step_by(2).max().unwrap();
+    if M > highest as u64 {
+        print!("{}", M);
+    } else {
+        print!("{}", highest);
+    }
 }

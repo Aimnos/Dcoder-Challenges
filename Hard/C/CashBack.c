28fb1a364cb88b58b2b2cac6e73974b4ed732e30
Aimnos <roikegami@hotmail.com>
2021-07-23 13:53:34 -0300

    Changed indentation to spaces

--- Hard/C/CashBack.c
+++ Hard/C/CashBack.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 
 int main() {
-	unsigned int N, Ci, lowest, highest;
-	unsigned long int M, Xi;
-	scanf("%u", &N);
-	scanf("%u %u", &highest, &lowest);
-	M = highest - lowest;
-	for(unsigned int i = 1; i < N; i++) {
-		scanf("%u %lu", &Ci, &Xi);
-		M += Ci - Xi;
-		if(Xi < lowest)
-			lowest = Xi;
+    unsigned int N, Ci, lowest, highest;
+    unsigned long int M, Xi;
+    scanf("%u", &N);
+    scanf("%u %u", &highest, &lowest);
+    M = highest - lowest;
+    for(unsigned int i = 1; i < N; i++) {
+        scanf("%u %lu", &Ci, &Xi);
+        M += Ci - Xi;
+        if(Xi < lowest)
+            lowest = Xi;
 
-		if(Ci > highest)
-			highest = Ci;
+        if(Ci > highest)
+            highest = Ci;
+    }
+    M += lowest;
+    if(M > highest)
+        printf("%lu", M);
+    else
+        printf("%lu", highest);
 
-	}
-	M += lowest;
-	if(M > highest)
-		printf("%lu", M);
-	else
-		printf("%lu", highest);
-
-	return 0;
+    return 0;
 }

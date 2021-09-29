e12e1de71542746ce0ee5206e67ed53f2f29bd4d
Aimnos <roikegami@hotmail.com>
2021-09-29 09:58:41 -0300

    Fixed failure for TestCases 1 and 2

--- Easy/Rust/The Largest Gap.rs
+++ Easy/Rust/The Largest Gap.rs
@@ -4,11 +4,13 @@ fn main() {
     let stdin = io::stdin();
     let mut lines = stdin.lock().lines();
     let N: u16 = lines.next().unwrap().unwrap().trim().parse().unwrap();
-    let Array: Vec<u16> = lines
+    // Even though the problem states that the maximum value for any element is 10000, using u16 fails for TestCase#1 and TestCase#2
+    let Array: Vec<u32> = lines
         .next()
         .unwrap()
         .unwrap()
         .split_whitespace()
+        .take(N as usize)
         .map(|element| element.parse().unwrap())
         .collect();
     print!(
